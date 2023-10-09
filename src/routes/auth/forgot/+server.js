be9a98863e26c17dbbb17d7fa718be3fab051420
Le Duc Thien <timducle@yahoo.com>
2023-10-09 17:17:23 +0700

    0.10

--- src/routes/auth/forgot/+server.js
+++ src/routes/auth/forgot/+server.js
@@ -1,4 +1,6 @@
 import { findEmail } from '$lib/stores/db';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '$env/static/private';
 
 export async function POST(event) {
   let requestBody = await event.request.json();
