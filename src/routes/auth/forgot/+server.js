6dd7c5149d1a75e8f4c97fd36ce45f3e27d66090
Thien Le <timducle@yahoo.com>
2023-10-11 15:18:05 +0000

    0.12

--- src/routes/auth/forgot/+server.js
+++ src/routes/auth/forgot/+server.js
@@ -1,5 +1,6 @@
 import { findEmail } from '$lib/stores/db';
 import jwt from 'jsonwebtoken';
+import Brevo from '@getbrevo/brevo';
 import { JWT_SECRET, BREVO_API_KEY } from '$env/static/private';
 
 export async function POST(event) {
@@ -7,6 +8,9 @@ export async function POST(event) {
   let user = await findEmail(requestBody.email);
   if(user) {
     let token = jwt.sign({subject:user.id}, JWT_SECRET, { expiresIn: '30m'}); // Create a token expiring in 30 minutes
+    let defaultClient = Brevo.ApiClient.instance;
+    defaultClient.authentications['api-key'].apiKey = BREVO_API_KEY;
+    let apiInstance = new Brevo.TransactionalEmailsApi();
   } else {
 
   }
