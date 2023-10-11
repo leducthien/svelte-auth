217a92085f8a7a4aae1d2e134e6c8289625a0048
Le Duc Thien <timducle@yahoo.com>
2023-10-11 17:27:18 +0700

    0.11

--- src/routes/auth/forgot/+server.js
+++ src/routes/auth/forgot/+server.js
@@ -1,12 +1,12 @@
 import { findEmail } from '$lib/stores/db';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '$env/static/private';
+import { JWT_SECRET, BREVO_API_KEY } from '$env/static/private';
 
 export async function POST(event) {
   let requestBody = await event.request.json();
   let user = await findEmail(requestBody.email);
   if(user) {
-
+    let token = jwt.sign({subject:user.id}, JWT_SECRET, { expiresIn: '30m'}); // Create a token expiring in 30 minutes
   } else {
 
   }
