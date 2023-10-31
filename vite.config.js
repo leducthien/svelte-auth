528839a62b43a71d2cf2231dee07e7ba5892505e
Le Duc Thien <timducle@yahoo.com>
2023-10-31 21:54:12 +0700

    0.21

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+// npm i -D @esbuild-plugins/node-globals-polyfill
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+// npm i -D @esbuild-plugins/node-modules-polyfill
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: 'globalThis'
+			},
+			// Enable esbuild polyfill plugins
+			plugins: [NodeGlobalsPolyfillPlugin(), NodeModulesPolyfillPlugin()]
+		}
+	}
 });
