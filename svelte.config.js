48d91c75b8474f07ba8791230b231915a35a6e43
Le Duc Thien <timducle@yahoo.com>
2023-11-03 22:24:45 +0700

    0.22

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 
 const config = {
 	kit: {
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-// npm i -D @esbuild-plugins/node-globals-polyfill
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-// npm i -D @esbuild-plugins/node-modules-polyfill
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		esbuildOptions: {
-			// Node.js global to browser globalThis
-			define: {
-				global: 'globalThis'
-			},
-			// Enable esbuild polyfill plugins
-			plugins: [NodeGlobalsPolyfillPlugin(), NodeModulesPolyfillPlugin()]
-		}
-	}
+	plugins: [sveltekit()]
 });
