e852b5e29045ad594e015a5d03fada6ee55b9ef4
Le Duc Thien <timducle@yahoo.com>
2024-03-03 21:29:57 +0700

    0.36

---
+++ db-init.sql
@@ -0,0 +1,15 @@
+-- Invoke this script with 
+-- PGPASSWORD='FrebuMIju2' psql -d postgres -f db-init.sql -h 127.0.0.1 -p 5443 -U postgres
+
+-- Create role for authetication
+DO $do$
+BEGIN
+  IF NOT EXISTS (
+    SELECT rolname
+    FROM pg_catalog.pg_roles
+    WHERE rolname='auth'
+  ) THEN CREATE ROLE auth;
+  END IF;
+END
+$do$;
+
