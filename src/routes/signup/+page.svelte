a36689cb73a342c6c6f415d7ffb1d73d5200b2fa
Le Duc Thien <timducle@yahoo.com>
2024-03-20 21:39:57 +0700

    0.48

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,4 +1,15 @@
-
+<script>
+  let email, password;
+  async function signup() {
+    if(!email) { // Tell user email cannot be empty
+      return;
+    }
+    if(!password) {
+      return;
+    }
+    let response = await fetch('/auth/signup');
+  }
+</script>
 
 <h1>Sign up</h1>
 
@@ -6,9 +17,17 @@
   <div>
     <label>
       Email
-      <input type="email" placeholder="Email" required />
+      <input type="email" placeholder="Email" required bind:value={email} />
     </label>
   <div id="email-error" />
   </div>
-
+  <div>
+    <label>
+      Password
+      <input type="password" required bind:value={password} />
+    </label>
+    <div id="password-error" />
+  </div>
+  <button on:click|preventDefault={signup}>Sign up</button>
 </form>
+<a href='/login'>Already have an account. Log in</a>
